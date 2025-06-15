1c55a0ccd326f801e5275bbeafd6df7983e82ed1
brainlesslukas <lukas.haible@web.de>
2025-06-15 12:36:47 +0200

    Refactoring

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,38 @@
+import Logo from "../../assets/ColorPalatte.svg";
+import { NavbarItems } from "./NavbarItems";
+
+const item = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About",
+    link: "/about",
+  },
+  {
+    name: "Docs",
+    link: "/docs",
+  },
+];
+
 const Navbar = () => {
-  return <div className="bg-red-700">Navbar</div>;
+  return (
+    <div className="w-full flex items-center bg-amber-300 justify-center">
+      <div className="w-[1300px] flex items-center">
+        <div className="p-2 flex items-center justify-center flex-1">
+          <img src={Logo} className="w-[50px]" />
+          <h1 className="pl-1 font-extrabold text-2xl">PixelDB</h1>
+        </div>
+
+        <NavbarItems item={item} />
+
+        <div className="p-2 flex items-center justify-center text-xl font-medium flex-1">
+          <a>Login</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
