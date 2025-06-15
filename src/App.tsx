276425d847f882ba046ff9b4068fc0723c6962fc
brainlesslukas <info@brainlesslukas.xyz>
2025-06-15 17:44:22 +0200

    Finished Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import NavBar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <NavBar />
-      <h1>PixelDB</h1>
+      <h1 className="h-[90vh] text-7xl font-extrabold content-center text-center">
+        PixelDB Placeholder
+      </h1>
+      <Footer />
     </>
   );
 }
