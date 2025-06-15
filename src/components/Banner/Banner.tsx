570296fabf701e28726626809b7444dcc48eff31
brainlesslukas <lukas.haible@web.de>
2025-06-15 21:44:46 +0200

    Banner in middle of Page

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,9 @@
+const Banner = () => {
+  return (
+    <div className="h-[80vh] bg-amber-200 max-w-[1300px] w-full flex items-center justify-center">
+      <h1>Welcome to PixelDB</h1>
+    </div>
+  );
+};
+
+export default Banner;
