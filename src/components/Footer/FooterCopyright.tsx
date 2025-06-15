c0dcb15d7c1aee8cf589c6fdd5adb53f22a3a6ba
brainlesslukas <lukas.haible@web.de>
2025-06-15 17:47:17 +0200

    Refactoring

---
+++ src/components/Footer/FooterCopyright.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FooterCopyright = () => {
+  return (
+    <div>
+      <h1>© 2025 PixelDB. All pixel rights reserved.</h1>
+    </div>
+  );
+};
+
+export default FooterCopyright;
