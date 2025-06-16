a0f26871fedbc12c57033b02baf6b72fe26fbe73
brainlesslukas <info@brainlesslukas.xyz>
2025-06-16 10:38:00 +0200

    Add Picture and first Design of Banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,7 +1,27 @@
+import Mario from "../../assets/Mario.svg";
+
 const Banner = () => {
   return (
-    <div className="h-[80vh] bg-amber-200 max-w-[1300px] w-full flex items-center justify-center">
-      <h1>Welcome to PixelDB</h1>
+    <div className="h-[80vh] max-w-[1000px] w-full flex items-center justify-between px-2">
+      <div className="flex flex-col space-y-1 max-w-xl">
+        <h1 className="text-6xl font-normal text-green-500">
+          Welcome to <br />
+          <span className="text-8xl font-extrabold text-blue-700 leading-tight">
+            PixelDB
+          </span>
+        </h1>
+        <p className="text-xl text-gray-700">
+          The ultimate database for all your pixel art and color palette needs.
+          Organize, explore, and share your creations with the world.
+        </p>
+      </div>
+      <div className="flex items-center justify-center">
+        <img
+          src={Mario}
+          className="h-[400px] w-auto object-contain animate-float"
+          alt="Color Palette"
+        />
+      </div>
     </div>
   );
 };
