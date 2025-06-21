bff9dfaae73196a104519de307b8185f847ee3bd
Lukas Haible <lukas.haible@web.de>
2025-06-21 16:56:15 +0200

    Close Button Logic

--- src/components/ui/Alert/Alert.tsx
+++ src/components/ui/Alert/Alert.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FaInfo } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
 
@@ -6,16 +7,23 @@ interface Props {
 }
 
 const Alert = ({ children }: Props) => {
+  const [isVisible, setIsVisible] = useState(true);
+
   return (
-    <div className="fixed w-full bottom-15 flex justify-center">
-      <div className="max-w-[500px] border-4 border-accent px-2 py-2 rounded-xl bg-background text-center flex items-center justify-center shadow-lg">
-        <FaInfo className="text-xl text-accent" />
-        <span className="text-base">{children}</span>
-        <div className="pl-4 pr-2 cursor-pointer">
-          <IoMdClose />
+    isVisible && (
+      <div className="fixed w-full bottom-15 flex justify-center">
+        <div className="max-w-[500px] border-4 border-accent px-2 py-2 rounded-xl bg-background text-center flex items-center justify-center shadow-lg">
+          <FaInfo className="text-xl text-accent" />
+          <span className="text-base">{children}</span>
+          <div
+            className="pl-4 pr-2 cursor-pointer"
+            onClick={() => setIsVisible(false)}
+          >
+            <IoMdClose />
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
