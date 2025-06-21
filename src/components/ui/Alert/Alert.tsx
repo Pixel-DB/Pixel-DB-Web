45527991e74f4d7e1826bc31ebe5e3e11bb52c20
Lukas <info@brainlesslukas.xyz>
2025-06-21 16:53:49 +0200

    Remove Button

---
+++ src/components/ui/Alert/Alert.tsx
@@ -0,0 +1,22 @@
+import { FaInfo } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Alert = ({ children }: Props) => {
+  return (
+    <div className="fixed w-full bottom-15 flex justify-center">
+      <div className="max-w-[500px] border-4 border-accent px-2 py-2 rounded-xl bg-background text-center flex items-center justify-center shadow-lg">
+        <FaInfo className="text-xl text-accent" />
+        <span className="text-base">{children}</span>
+        <div className="pl-4 pr-2 cursor-pointer">
+          <IoMdClose />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Alert;
