8502b8ea4491c4e1d3237661e26b7948dac821d4
Lukas Haible <lukas.haible@web.de>
2025-07-05 15:01:59 +0200

    create useData hook and connect with axios

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,5 +1,19 @@
-import axios from "axios";
+import { type AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+import apiClient from "@/services/api-client";
 
-export default axios.create({
-  baseURL: "http://localhost:3000",
-});
+const [data, setData] = useState<AxiosResponse | null | void>(null);
+const useData = () => {
+  useEffect(() => {
+    apiClient
+      .get("/")
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return data;
+};
+
+export default useData;
