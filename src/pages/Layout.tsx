0824362f879a6cbc3a104e119e482d138d3e74ac
Lukas <info@brainlesslukas.xyz>
2025-07-06 20:11:54 +0200

    Login Card Border

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,5 +1,21 @@
+import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/Navbar/Navbar";
+import Alert from "@/components/ui/Alert/Alert";
+import { Outlet } from "react-router-dom";
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+
+      <main className="flex-grow">
+        <Outlet />
+        <Alert>This Service is still under Production</Alert>
+      </main>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
