3ea90ec7d967a221c0be09d7b3009a2289b4853a
Lukas Haible <lukas.haible@web.de>
2025-07-06 20:11:54 +0200

    Login Card Border

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -4,9 +4,10 @@ interface Props {
   children: React.ReactNode;
   sm?: boolean;
   color?: "blue" | "green" | "red" | "amber" | "accent" | "secondary";
+  href?: string;
 }
 
-function Button({ children, sm = false, color = "blue" }: Props) {
+function Button({ children, sm = false, color = "blue", href = "#" }: Props) {
   const bgColorClasses = {
     blue: "bg-blue-500",
     green: "bg-green-500",
@@ -18,7 +19,7 @@ function Button({ children, sm = false, color = "blue" }: Props) {
 
   return (
     <a
-      href="#"
+      href={href}
       className={`${bgColorClasses[color]} ${
         sm ? "px-2 py-1" : "px-4 py-2"
       } rounded-lg border-slate-800 border-2 font-bold text-slate-800 button-3d w-full ${
