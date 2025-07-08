d8bcf9e35ee9cc4c392c5d6f6bffbcc047a33777
Lukas <lukas.haible@web.de>
2025-07-08 19:34:17 +0200

    Login Page Design

--- src/components/ui/TextInput/Input.tsx
+++ src/components/ui/TextInput/Input.tsx
@@ -7,11 +7,13 @@ interface Props {
 const Input = ({ type, name, label }: Props) => {
   return (
     <form>
-      <label htmlFor={name}>{label}</label>
+      <label htmlFor={name} className="text-slate-800">
+        {label}
+      </label>
       <input
         name={name}
         type={type}
-        className="border-2 border-secondary focus:outline focus:outline-secondary rounded p-2  max-w-[400px] w-full"
+        className="border-2 border-secondary focus:outline focus:outline-secondary rounded p-2  max-w-[400px] w-full "
         placeholder={
           type === "email" ? "example@email.com" : "Please enter your password"
         }
