56c689d045df16e7eb3dc8ad7e6e9179d6abe190
Lukas Haible <lukas.haible@web.de>
2025-07-08 19:34:17 +0200

    Login Page Design

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -5,9 +5,16 @@ interface Props {
   sm?: boolean;
   color?: "blue" | "green" | "red" | "amber" | "accent" | "secondary";
   href?: string;
+  wFull?: boolean;
 }
 
-function Button({ children, sm = false, color = "blue", href = "#" }: Props) {
+function Button({
+  children,
+  sm = false,
+  color = "blue",
+  href = "#",
+  wFull = false,
+}: Props) {
   const bgColorClasses = {
     blue: "bg-blue-500",
     green: "bg-green-500",
@@ -23,7 +30,7 @@ function Button({ children, sm = false, color = "blue", href = "#" }: Props) {
       className={`${bgColorClasses[color]} ${
         sm ? "px-2 py-1" : "px-4 py-2"
       } rounded-lg border-slate-800 border-2 font-bold text-slate-800 button-3d w-full ${
-        sm ? "max-w-[100px]" : "max-w-[300px]"
+        sm ? "max-w-[100px]" : wFull ? "w-full" : "max-w-[300px]"
       } text-center`}
     >
       {children}
