96a39d7c17a650c40e7dd47c7632dcfac17a33e1
Lukas Haible <lukas.haible@web.de>
2025-07-08 20:05:39 +0200

    Add Username, First Name, LastName to UI Component (Input Field)

--- src/components/ui/TextInput/Input.tsx
+++ src/components/ui/TextInput/Input.tsx
@@ -1,10 +1,18 @@
 interface Props {
-  type: "email" | "password";
+  type: "email" | "password" | "firstName" | "lastName";
   name: string;
   label?: string;
 }
 
 const Input = ({ type, name, label }: Props) => {
+  const placeholderText = {
+    email: "john.doe@gmail.com",
+    password: "Please enter your password",
+    firstName: "John",
+    lastName: "Doe",
+    username: "john_doe123",
+  };
+
   return (
     <form>
       <label htmlFor={name} className="text-slate-800">
@@ -14,9 +22,7 @@ const Input = ({ type, name, label }: Props) => {
         name={name}
         type={type}
         className="border-2 border-secondary focus:outline focus:outline-secondary rounded p-2  max-w-[400px] w-full "
-        placeholder={
-          type === "email" ? "example@email.com" : "Please enter your password"
-        }
+        placeholder={placeholderText[type]}
       />
     </form>
   );
