e5f25adb067406ecb9599e84d29ac03b8f4a8901
Lukas Haible <info@brainlesslukas.xyz>
2025-07-08 22:32:06 +0200

    Get Input Value from Input Value

--- src/components/ui/TextInput/Input.tsx
+++ src/components/ui/TextInput/Input.tsx
@@ -1,10 +1,15 @@
+import { useRef } from "react";
+
 interface Props {
   type: "email" | "password" | "firstName" | "lastName" | "username";
   name: string;
   label?: string;
+  onInput: (value: string) => void;
 }
 
-const Input = ({ type, name, label }: Props) => {
+const Input = ({ type, name, label, onInput }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   const placeholderText = {
     email: "john.doe@gmail.com",
     password: "Please enter your password",
@@ -14,17 +19,19 @@ const Input = ({ type, name, label }: Props) => {
   };
 
   return (
-    <form>
+    <>
       <label htmlFor={name} className="text-slate-800">
         {label}
       </label>
       <input
+        ref={ref}
         name={name}
         type={type}
         className="border-2 border-secondary focus:outline focus:outline-secondary rounded p-2  max-w-[400px] w-full "
         placeholder={placeholderText[type]}
+        onChange={() => onInput(ref.current?.value || "")}
       />
-    </form>
+    </>
   );
 };
 
