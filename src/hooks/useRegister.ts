74acef1c0bae5976c437fcb80e7803a2bcefd00c
Lukas Haible <info@brainlesslukas.xyz>
2025-07-29 12:39:16 +0200

    Connect to Backend

---
+++ src/hooks/useRegister.ts
@@ -0,0 +1,37 @@
+import apiClient from "@/services/api-client";
+import { useState } from "react";
+
+interface RegisterData {
+  email: string;
+  password: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+}
+
+const useRegister = () => {
+  const [statusCode, setStatusCode] = useState<number | null>(null);
+
+  const register = async (credentials: RegisterData) => {
+    await apiClient
+      .post("/auth/register", {
+        email: credentials.email,
+        password: credentials.password,
+        username: credentials.username,
+        firstName: credentials.firstName,
+        lastName: credentials.lastName,
+      })
+      .then(function (response) {
+        console.log(response);
+        setStatusCode(response.status);
+        localStorage.setItem("token", response.data.data.Token);
+      })
+      .catch(function (error) {
+        setStatusCode(error.status);
+      });
+  };
+
+  return { register, statusCode };
+};
+
+export default useRegister;
