55f6bcbfae4e491a80bdce90532840f5718292fc
Lukas <info@brainlesslukas.xyz>
2025-08-03 22:57:26 +0200

    Add .env to gitignore

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.178.111:3000", //Change to ur backend URL
+  baseURL: "http://192.168.178.112:3000", //Change to ur backend URL
 });
