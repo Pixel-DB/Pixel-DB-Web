e4f6f032ff39c5fba0e92d1a6fb8b29af9882090
Lukas <lukas.haible@web.de>
2025-08-04 22:44:12 +0200

    remove test API Call

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,11 @@ import Banner from "@/components/Banner/Banner";
 import useUser from "@/hooks/useUser";
 
 const Home = () => {
-  const { userData } = useUser();
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-center">
         <Banner />
       </div>
-      <div>{userData?.Email}</div>
     </div>
   );
 };
