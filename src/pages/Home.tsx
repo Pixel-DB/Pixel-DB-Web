0c13156b819a255bf35563676782de3b7f3f574a
Lukas <info@brainlesslukas.xyz>
2025-08-04 22:39:20 +0200

    Fetch User Data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import Banner from "@/components/Banner/Banner";
+import useUser from "@/hooks/useUser";
 
 const Home = () => {
+  const { userData } = useUser();
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-center">
         <Banner />
       </div>
+      <div>{userData?.Email}</div>
     </div>
   );
 };
