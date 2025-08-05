1eabc179729fe284b8b5a7f86c3b82a6d6dd43b4
Lukas <lukas.haible@web.de>
2025-08-06 00:46:17 +0200

    PixelArtCard

--- src/pages/PixelArt.tsx
+++ src/pages/PixelArt.tsx
@@ -1,15 +1,10 @@
+import PixelArtCard from "@/components/PixelArtCard/PixelArtCard";
+
 const PixelArt = () => {
   return (
     <div className="flex items-center justify-center">
       <div className="grid grid-cols-3 gap-6">
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
+        <PixelArtCard />
       </div>
     </div>
   );
