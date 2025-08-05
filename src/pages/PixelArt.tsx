1af119081ea5bba8d6019f3504a966e9ae90b7d1
Lukas <info@brainlesslukas.xyz>
2025-08-06 00:31:50 +0200

    Grid

--- src/pages/PixelArt.tsx
+++ src/pages/PixelArt.tsx
@@ -1,5 +1,18 @@
 const PixelArt = () => {
-  return <div>PixelArt</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <div className="grid grid-cols-3 gap-6">
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+      </div>
+    </div>
+  );
 };
 
 export default PixelArt;
