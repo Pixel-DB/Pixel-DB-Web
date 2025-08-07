c804f5c473e32aded35fb17c387ba88f7498af88
Lukas Haible <lukas.haible@web.de>
2025-08-07 20:33:33 +0200

    API + Design Fixes

--- src/pages/PixelArt.tsx
+++ src/pages/PixelArt.tsx
@@ -5,9 +5,14 @@ const PixelArt = () => {
   const { PixelArtData } = usePixelArt();
   return (
     <div className="flex items-center justify-center">
-      <div className="grid grid-cols-3 gap-6">
-        <PixelArtCard />
-        {PixelArtData?.Status}
+      <div className="grid grid-cols-4 gap-6">
+        {PixelArtData?.Data.items.map((item) => (
+          <PixelArtCard
+            key={item.ID}
+            name={item.Name}
+            description={item.Description}
+          />
+        ))}
       </div>
     </div>
   );
