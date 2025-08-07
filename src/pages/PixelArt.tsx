5271d6dfac23ee096d627d6e00431bb22db94a17
Lukas Haible <lukas.haible@web.de>
2025-08-07 22:25:50 +0200

    Pagination Buttons

--- src/pages/PixelArt.tsx
+++ src/pages/PixelArt.tsx
@@ -1,4 +1,5 @@
 import PixelArtCard from "@/components/PixelArtCard/PixelArtCard";
+import Button from "@/components/ui/Button/Button";
 import usePixelArt from "@/hooks/usePixelArt";
 
 const PixelArt = () => {
@@ -8,18 +9,31 @@ const PixelArt = () => {
     setPage(page + 1);
   };
 
+  const handlePreviousPage = () => {
+    setPage(page - 1);
+  };
+
   return (
-    <div className="flex items-center justify-center">
-      <div className="grid 2xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-6">
-        {PixelArtData?.Data.items.map((item) => (
-          <PixelArtCard
-            key={item.ID}
-            name={item.Name}
-            description={item.Description}
-          />
-        ))}
-        {page}
-        <button onClick={handleNextPage}>Next Page</button>
+    <div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="grid 2xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-6">
+          {PixelArtData?.Data.items.map((item) => (
+            <PixelArtCard
+              key={item.ID}
+              name={item.Name}
+              description={item.Description}
+            />
+          ))}
+        </div>
+        <div className="mt-10 flex gap-6 items-center justify-center">
+          <Button disabled w-full color="blue" onClick={handlePreviousPage}>
+            Back
+          </Button>
+          <h1 className="text-xl font-bold">{page}</h1>
+          <Button wFull color="amber" onClick={handleNextPage}>
+            Next
+          </Button>
+        </div>
       </div>
     </div>
   );
