48b88710951b835b2fc9b44e26655e6728dfb4ac
Lukas Haible <lukas.haible@web.de>
2025-08-08 12:30:27 +0200

    Get ID

--- src/components/PixelArtCard/PixelArtCard.tsx
+++ src/components/PixelArtCard/PixelArtCard.tsx
@@ -4,17 +4,26 @@ import "./PixelArtCard.css";
 interface Props {
   name: string;
   description: string;
+  id: string;
 }
 
-const PixelArtCard = ({ name, description }: Props) => {
+const PixelArtCard = ({ name, description, id }: Props) => {
+  const handleClick = () => {
+    console.log("ID" + id);
+  };
+
   return (
-    <div className="rounded-lg shadow m-2 max-w-[300px] max-h-[450px]">
+    <div
+      className="rounded-lg shadow m-2 max-w-[300px] max-h-[450px]"
+      onClick={handleClick}
+    >
       <div className="bg-white w-[250px] h-[250px] rounded-lg flex justify-center items-center object-cover">
         <img className="p-3" src={Placeholder} alt="PixelArt" />
       </div>
       <div className="px-2 pb-2">
         <h1 className="text-black text-lg">{name}</h1>
         <p className="text-gray-700 text-sm">{description}</p>
+        <p className="text-gray-700 text-xs">{id}</p>
       </div>
     </div>
   );
