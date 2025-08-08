a12ab4ad4d4e35b78c348ba8041a2a76eee3bf95
Lukas <info@brainlesslukas.xyz>
2025-08-08 22:37:12 +0200

    Fetch Data

--- src/pages/PixelArtDetail.tsx
+++ src/pages/PixelArtDetail.tsx
@@ -1,5 +1,22 @@
+import usePixelArtDetail from "@/hooks/usePixelArtDetail";
+
 const PixelArtDetail = () => {
-  return <div>PixelArtDetail</div>;
+  const { PixelArtDetailData } = usePixelArtDetail();
+  return (
+    <div className="flex w-full h-full justify-center items-center flex-row gap-24">
+      <div className="w-[600px] h-[600px] objekt-cover border-2 rounded-md my-20">
+        <img
+          src={`http://localhost:3000/pixelart/${PixelArtDetailData?.Data.ID}/picture`}
+        />
+      </div>
+      <div className="flex flex-col justify-center">
+        <div>{PixelArtDetailData?.Data.Name}</div>
+        <div>{PixelArtDetailData?.Data.Description}</div>
+        <div>{PixelArtDetailData?.Data.CreatedAt}</div>
+        <div>{PixelArtDetailData?.Data.ID}</div>
+      </div>
+    </div>
+  );
 };
 
 export default PixelArtDetail;
