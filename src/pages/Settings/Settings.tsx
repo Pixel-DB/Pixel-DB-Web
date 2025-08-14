d5f90e22caa57e321f789e3df65ee1165fd9a31b
Lukas <lukas.haible@web.de>
2025-08-14 22:50:17 +0200

    Refactoring

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -3,10 +3,10 @@ import SettingsSideBar from "@/components/Settings/SettingsSideBar";
 const Settings = () => {
   return (
     <div className="flex h-full w-full">
-      <aside className="max-w-[200px] w-full">
+      <aside className="max-w-[200px] w-full shadow-xl/30 h-screen mr-2">
         <SettingsSideBar />
       </aside>
-      <div className="bg-green-400 w-full">Settings Content</div>
+      <div className="w-full">Settings Content</div>
     </div>
   );
 };
