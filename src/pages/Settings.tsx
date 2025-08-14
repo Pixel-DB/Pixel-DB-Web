4900835eeebb0446fa858e720c7462508e816e1f
Lukas Haible <info@brainlesslukas.xyz>
2025-08-14 21:29:32 +0200

    Design

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,14 @@
+import SettingsSideBar from "@/components/Settings/SettingsSideBar";
+
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div className="flex h-full w-full">
+      <aside className="max-w-[200px] w-full">
+        <SettingsSideBar />
+      </aside>
+      <div className="bg-green-400 w-full">Settings Content</div>
+    </div>
+  );
 };
 
 export default Settings;
