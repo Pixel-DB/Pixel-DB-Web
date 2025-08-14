fcb2b16ad5071d8df602a705bc8caf92f40d723f
Lukas Haible <info@brainlesslukas.xyz>
2025-08-14 23:15:58 +0200

    Refactoring Layout + Routes

---
+++ src/pages/Settings/SettingsLayout.tsx
@@ -0,0 +1,15 @@
+import SettingsSideBar from "@/components/Settings/SettingsSideBar";
+import { Outlet } from "react-router-dom";
+
+const SettingsLayout = () => {
+  return (
+    <div className="flex h-full w-full">
+      <aside className="max-w-[200px] w-full shadow-xl/30 h-screen mr-2">
+        <SettingsSideBar />
+      </aside>
+      <Outlet />
+    </div>
+  );
+};
+
+export default SettingsLayout;
