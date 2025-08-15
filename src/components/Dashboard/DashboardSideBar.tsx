e6dc7adc3c6a15fa21491fc92b440c48e80bd86f
Lukas Haible <lukas.haible@web.de>
2025-08-16 00:31:59 +0200

    Function

--- src/components/Dashboard/DashboardSideBar.tsx
+++ src/components/Dashboard/DashboardSideBar.tsx
@@ -1,14 +1,34 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const DashboardSideBar = () => {
   return (
     <div className="flex flex-col gap-8 p-4 text-xl text-gray-800">
       <h1 className="text-2xl font-bold py-4 text-black">Dashboard</h1>
-      <Link to="/Dashboard">Home</Link>
-      <Link to="/Dashboard/profile">Profile</Link>
-      <Link to="/Dashboard/upload">Upload</Link>
-      <Link to="/Dashboard/settings">Settings</Link>
-      <Link to="/Dashboard/license">License</Link>
+      <NavLink to="/Dashboard" end>
+        {({ isActive }) => (
+          <span className={isActive ? "bg-blue-500" : ""}>Home</span>
+        )}
+      </NavLink>
+      <NavLink to="/Dashboard/profile">
+        {({ isActive }) => (
+          <span className={isActive ? "bg-blue-500" : ""}>Profile</span>
+        )}
+      </NavLink>
+      <NavLink to="/Dashboard/upload">
+        {({ isActive }) => (
+          <span className={isActive ? "bg-blue-500" : ""}>Upload</span>
+        )}
+      </NavLink>
+      <NavLink to="/Dashboard/settings">
+        {({ isActive }) => (
+          <span className={isActive ? "bg-blue-500" : ""}>Settings</span>
+        )}
+      </NavLink>
+      <NavLink to="/Dashboard/license">
+        {({ isActive }) => (
+          <span className={isActive ? "bg-blue-500" : ""}>License</span>
+        )}
+      </NavLink>
     </div>
   );
 };
