38dd22e91ba2b0e7a096aa98bc5ef097b45a96f2
Lukas Haible <info@brainlesslukas.xyz>
2025-08-16 00:19:00 +0200

    Links + Logo navigates to home

--- src/components/Navbar/NavbarLogo.tsx
+++ src/components/Navbar/NavbarLogo.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 import Logo from "../../assets/ColorPalatte.svg";
 
 const NavbarLogo = () => {
   return (
-    <div className="p-2 flex items-center justify-center flex-1">
+    <Link className="p-2 flex items-center justify-center flex-1" to={"/"}>
       <img src={Logo} className="w-[50px]" />
       <h1 className="pl-1 font-extrabold text-2xl">PixelDB</h1>
-    </div>
+    </Link>
   );
 };
 
