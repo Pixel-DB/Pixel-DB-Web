80361e43ba92d994d4083b8c35d2f1407c2b1900
Lukas <lukas.haible@web.de>
2025-08-15 23:14:26 +0200

    Design

--- src/pages/Settings/SettingsLayout.tsx
+++ src/pages/Settings/SettingsLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const SettingsLayout = () => {
   return (
     <div className="flex h-full w-full">
-      <aside className="max-w-[200px] w-full shadow-xl/30 h-screen mr-2">
+      <aside className="z-999 max-w-[200px] w-full shadow-xl/30 h-screen mr-2">
         <SettingsSideBar />
       </aside>
       <div className="absolute flex w-full justify-center">
