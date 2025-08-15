80aa5b92d8f18ee2cf2d14fcf58e201174cd11a6
Lukas <info@brainlesslukas.xyz>
2025-08-15 23:09:44 +0200

    Desgin + Fix Links Bug

--- src/components/Settings/SettingsSideBar.tsx
+++ src/components/Settings/SettingsSideBar.tsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 const SettingsSideBar = () => {
   return (
     <div className="flex flex-col gap-8 p-4 text-xl text-gray-800">
       <h1 className="text-2xl font-bold py-4 text-black">Settings</h1>
       <a href="/settings">Home</a>
-      <a href="/settings/profil">Profil</a>
-      <a href="#">General</a>
-      <a href="#">Pixel Arts</a>
-      <a href="#">License</a>
+      <Link to="/settings/profile">Profile</Link>
+      <Link to="#">General</Link>
+      <Link to="#">Pixel Arts</Link>
+      <Link to="#">License</Link>
     </div>
   );
 };
