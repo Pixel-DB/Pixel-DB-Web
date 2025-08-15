88648de2cbd3208c870ff12c52e20d4bbce2fde7
Lukas <info@brainlesslukas.xyz>
2025-08-15 23:14:26 +0200

    Design

--- src/components/Settings/SettingsSideBar.tsx
+++ src/components/Settings/SettingsSideBar.tsx
@@ -4,11 +4,11 @@ const SettingsSideBar = () => {
   return (
     <div className="flex flex-col gap-8 p-4 text-xl text-gray-800">
       <h1 className="text-2xl font-bold py-4 text-black">Settings</h1>
-      <a href="/settings">Home</a>
+      <Link to="/settings">Home</Link>
       <Link to="/settings/profile">Profile</Link>
-      <Link to="#">General</Link>
-      <Link to="#">Pixel Arts</Link>
-      <Link to="#">License</Link>
+      <Link to="/settings/general">General</Link>
+      <Link to="/settings/pixelarts">Pixel Arts</Link>
+      <Link to="/settings/license">License</Link>
     </div>
   );
 };
