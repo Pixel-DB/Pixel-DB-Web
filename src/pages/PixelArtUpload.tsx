754d05beeb02405d79cd2a869d2f850bb0fed6c7
Lukas Haible <lukas.haible@web.de>
2025-08-16 15:29:47 +0200

    Size

--- src/pages/PixelArtUpload.tsx
+++ src/pages/PixelArtUpload.tsx
@@ -25,43 +25,41 @@ const PixelArtUpload = () => {
   };
 
   return (
-    <div className="flex justify-center items-center p-16">
-      <div className="border-3d border-2 border-slate-800 rounded-md max-w-[800px] w-full flex flex-col">
-        <h1 className="w-full text-2xl text-left font-bold p-4">
-          Upload PixelArt
-        </h1>
-        <form
-          className="flex flex-col gap-4 p-4 w-full"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className="flex flex-row gap-4">
-            <Input type="text" label="Name" {...register("Name")} />
-            <Input type="text" label="Author" {...register("Author")} />
-          </div>
-          <TextArea
-            placeholder="Enter Description here"
-            label="Description"
-            rows={6}
-            {...register("Description")}
+    <div className="flex justify-center items-center m-10 border-3d border-2 border-slate-800 rounded-md max-w-[700px] w-full flex-col">
+      <h1 className="w-full text-2xl text-left font-bold p-4">
+        Upload PixelArt
+      </h1>
+      <form
+        className="flex flex-col gap-4 p-4 w-full"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="flex flex-row gap-4">
+          <Input type="text" label="Name" {...register("Name")} />
+          <Input type="text" label="Author" {...register("Author")} />
+        </div>
+        <TextArea
+          placeholder="Enter Description here"
+          label="Description"
+          rows={6}
+          {...register("Description")}
+        />
+        <div className="flex flex-row gap-4 w-1/2">
+          <Input
+            type="file"
+            accept="image/png"
+            label="Upload Pixel Art"
+            {...register("UploadPixelArt")}
           />
-          <div className="flex flex-row gap-4 w-1/2">
-            <Input
-              type="file"
-              accept="image/png"
-              label="Upload Pixel Art"
-              {...register("UploadPixelArt")}
-            />
-          </div>
-          <div className="flex flex-row-reverse w-full justify-between pt-6">
-            <Button wFull color="green" sm type="submit">
-              Submit
-            </Button>
-            <Button wFull color="accent" sm>
-              Reset
-            </Button>
-          </div>
-        </form>
-      </div>
+        </div>
+        <div className="flex flex-row-reverse w-full justify-between pt-6">
+          <Button wFull color="green" sm type="submit">
+            Submit
+          </Button>
+          <Button wFull color="accent" sm>
+            Reset
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
