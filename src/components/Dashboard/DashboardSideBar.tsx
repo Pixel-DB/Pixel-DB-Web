875272d0e106b769c5e6566ff0c878a2c8a139fa
Lukas Haible <info@brainlesslukas.xyz>
2025-08-16 12:17:38 +0200

    active buttons design

--- src/components/Dashboard/DashboardSideBar.tsx
+++ src/components/Dashboard/DashboardSideBar.tsx
@@ -6,27 +6,67 @@ const DashboardSideBar = () => {
       <h1 className="text-2xl font-bold py-4 text-black">Dashboard</h1>
       <NavLink to="/Dashboard" end>
         {({ isActive }) => (
-          <span className={isActive ? "bg-blue-500" : ""}>Home</span>
+          <span
+            className={
+              isActive
+                ? "bg-gray-300 px-4 py-2 flex rounded-md"
+                : "px-4 py-2 flex"
+            }
+          >
+            Home
+          </span>
         )}
       </NavLink>
       <NavLink to="/Dashboard/profile">
         {({ isActive }) => (
-          <span className={isActive ? "bg-blue-500" : ""}>Profile</span>
+          <span
+            className={
+              isActive
+                ? "bg-gray-300 px-4 py-2 flex rounded-md"
+                : "px-4 py-2 flex"
+            }
+          >
+            Profile
+          </span>
         )}
       </NavLink>
       <NavLink to="/Dashboard/upload">
         {({ isActive }) => (
-          <span className={isActive ? "bg-blue-500" : ""}>Upload</span>
+          <span
+            className={
+              isActive
+                ? "bg-gray-300 px-4 py-2 flex rounded-md"
+                : "px-4 py-2 flex"
+            }
+          >
+            Upload
+          </span>
         )}
       </NavLink>
       <NavLink to="/Dashboard/settings">
         {({ isActive }) => (
-          <span className={isActive ? "bg-blue-500" : ""}>Settings</span>
+          <span
+            className={
+              isActive
+                ? "bg-gray-300 px-4 py-2 flex rounded-md"
+                : "px-4 py-2 flex"
+            }
+          >
+            Settings
+          </span>
         )}
       </NavLink>
       <NavLink to="/Dashboard/license">
         {({ isActive }) => (
-          <span className={isActive ? "bg-blue-500" : ""}>License</span>
+          <span
+            className={
+              isActive
+                ? "bg-gray-300 px-4 py-2 flex rounded-md"
+                : "px-4 py-2 flex"
+            }
+          >
+            License
+          </span>
         )}
       </NavLink>
     </div>
