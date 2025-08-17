0a7f2ec38b981680c4a8931eb12eadd18f7b720c
Lukas Haible <lukas.haible@web.de>
2025-08-17 19:57:28 +0200

    Responsive Footer

--- src/components/Footer/FooterCopyright.tsx
+++ src/components/Footer/FooterCopyright.tsx
@@ -3,8 +3,12 @@ import Logo from "../../assets/ColorPalatte.svg";
 const FooterCopyright = () => {
   return (
     <div className="flex items-center">
-      <span>© 2025</span>
-      <img src={Logo} className="h-7 pl-2 pr-1" alt="PixelDB Logo" />
+      <span className="p-2 sm:block">© 2025</span>
+      <img
+        src={Logo}
+        className="h-7 pl-2 pr-1 hidden sm:block"
+        alt="PixelDB Logo"
+      />
       <span>PixelDB. All pixel rights reserved.</span>
     </div>
   );
