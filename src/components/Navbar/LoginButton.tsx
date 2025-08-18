19a871b8eb27725dca9e7acce77ab9edd93548a5
Lukas Haible <lukas.haible@web.de>
2025-08-18 23:37:40 +0200

    Menu Button

--- src/components/Navbar/LoginButton.tsx
+++ src/components/Navbar/LoginButton.tsx
@@ -2,11 +2,9 @@ import Button from "../ui/Button/Button";
 
 const LoginButton = () => {
   return (
-    <div className="p-2 flex items-center justify-center text-xl font-medium flex-1">
-      <Button sm color="accent" href="/auth/login">
-        Login
-      </Button>
-    </div>
+    <Button sm color="accent" href="/auth/login">
+      Login
+    </Button>
   );
 };
 
