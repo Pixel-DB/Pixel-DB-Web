9c2b940fe9b8bbbc9cdbc159a44229db46334b0c
Lukas <lukas.haible@web.de>
2025-08-19 22:41:35 +0200

    Start Exploring Button + Implement

--- src/components/Home/Banner/Banner.tsx
+++ src/components/Home/Banner/Banner.tsx
@@ -1,19 +1,27 @@
 import Mario from "../../../assets/Mario.svg";
+import Register from "../CTAButtons/Register";
+import StartExploring from "../CTAButtons/StartExploring";
 
 const Banner = () => {
   return (
     <div className="h-[80vh] max-w-[1000px] w-full flex items-center justify-between px-2">
-      <div className="flex flex-col space-y-1 max-w-xl">
-        <h1 className="text-5xl md:text-6xl font-normal text-text">
-          Welcome to <br />
-          <span className="text-7xl md:text-8xl font-extrabold text-secondary leading-tight">
-            PixelDB
-          </span>
-        </h1>
-        <p className="text-xl text-gray-700">
-          Pixel-BD is an open-source online platform where anyone can upload,
-          share, and showcase their pixel art creations with the community.
-        </p>
+      <div className="flex flex-col">
+        <div className="flex flex-col space-y-1 max-w-xl">
+          <h1 className="text-5xl md:text-6xl font-normal text-text">
+            Welcome to <br />
+            <span className="text-7xl md:text-8xl font-extrabold text-secondary leading-tight">
+              PixelDB
+            </span>
+          </h1>
+          <p className="text-xl text-gray-700">
+            Pixel-BD is an open-source online platform where anyone can upload,
+            share, and showcase their pixel art creations with the community.
+          </p>
+        </div>
+        <div className="w-3/4 flex flex-row gap-4 pt-5">
+          <StartExploring />
+          <Register />
+        </div>
       </div>
       <div className="hidden md:block items-center justify-center">
         <img
