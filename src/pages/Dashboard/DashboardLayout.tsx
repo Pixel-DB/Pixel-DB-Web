fe716241780d74a374ca7e487fa0bf4197cebaf3
Lukas <lukas.haible@web.de>
2025-08-19 21:37:50 +0200

    Dashboard

--- src/pages/Dashboard/DashboardLayout.tsx
+++ src/pages/Dashboard/DashboardLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const DashboardLayout = () => {
   return (
     <div className="flex h-full w-full">
-      <aside className="z-30 max-w-[200px] w-full shadow-xl/30 h-screen mr-2">
+      <aside className="z-30 max-w-[200px] w-full shadow-xl/30 h-screen mr-2 border-r-1 border-gray-700">
         <DashboardSideBar />
       </aside>
       <div className="absolute flex w-full justify-center">
