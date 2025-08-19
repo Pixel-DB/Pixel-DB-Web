76681999a547c44594478eec1a52542eec119636
Lukas <lukas.haible@web.de>
2025-08-19 16:07:18 +0200

    Hide Picture at home

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -4,9 +4,9 @@ const Banner = () => {
   return (
     <div className="h-[80vh] max-w-[1000px] w-full flex items-center justify-between px-2">
       <div className="flex flex-col space-y-1 max-w-xl">
-        <h1 className="text-6xl font-normal text-text">
+        <h1 className="text-5xl md:text-6xl font-normal text-text">
           Welcome to <br />
-          <span className="text-8xl font-extrabold text-secondary leading-tight">
+          <span className="text-7xl md:text-8xl font-extrabold text-secondary leading-tight">
             PixelDB
           </span>
         </h1>
@@ -15,7 +15,7 @@ const Banner = () => {
           share, and showcase their pixel art creations with the community.
         </p>
       </div>
-      <div className="flex items-center justify-center">
+      <div className="hidden md:block items-center justify-center">
         <img
           src={Mario}
           className="h-[400px] w-auto object-contain animate-float"
