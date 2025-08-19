224a8899d5c4d718fb331cbf23d2170aab22149f
Lukas <lukas.haible@web.de>
2025-08-19 21:41:34 +0200

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import FooterLinks from "./FooterLinks";
 
 const Footer = () => {
   return (
-    <div className="w-full flex items-center bg-primary justify-center p-4 text-center mt-auto">
+    <div className="w-full flex items-center bg-primary justify-center p-4 text-center mt-auto border-t-1 border-gray-500">
       <div className="w-full max-w-[1300px] flex flex-col md:flex-row items-center justify-between">
         <FooterCopyright />
 
