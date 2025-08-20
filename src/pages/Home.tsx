35c9f244276a6763388ad723b497b30c086853a3
Lukas <info@brainlesslukas.xyz>
2025-08-20 16:03:01 +0200

    Desgin Boxes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
 import Banner from "@/components/Home/Banner/Banner";
+import StatsBox from "@/components/Home/Stats/StatsBox";
 
 const Home = () => {
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-center w-full">
         <Banner />
+      </div>
+      <div className="grid grid-cols-4  max-w-[1200px] w-full mx-auto m-5">
+        <StatsBox name="Users" color="accent" />
+        <StatsBox name="Total Requests" color="amber-500" />
+        <StatsBox name="Pixel arts" color="secondary" />
+        <StatsBox name="Github Stars" color="green-500" />
       </div>
     </div>
   );
