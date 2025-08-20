1ea6c4a33cdba790a0749bcf6b97a40771187758
Lukas <info@brainlesslukas.xyz>
2025-08-20 23:56:50 +0200

    User Context

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+type User = {
+  CreatedAt: string;
+  Email: string;
+  ID: string;
+  FirstName: string;
+  LastName: string;
+  Username: string;
+  Role: string;
+};
+
+type UserContextType = {
+  user: User | null;
+  setUser: (user: User | null) => void;
+};
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserContext = () => {
+  const context = useContext(UserContext);
+  if (!context)
+    throw new Error("useUserContext must be used inside UserProvider");
+  return context;
+};
