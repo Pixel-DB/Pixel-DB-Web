49758ad8033000ce10c475dd5fed0334e413de98
Lukas Haible <lukas.haible@web.de>
2025-08-20 16:03:01 +0200

    Desgin Boxes

--- src/components/Home/Stats/StatsBox.tsx
+++ src/components/Home/Stats/StatsBox.tsx
@@ -1,9 +1,20 @@
+import "../../../App.css";
+
 interface Props {
   name: string;
+  count?: number;
+  color?: string;
 }
 
-const StatsBox = ({ name }: Props) => {
-  return <div className="w-64 h-48 bg-red-500">{name}</div>;
+const StatsBox = ({ name, color = "white", count = 0 }: Props) => {
+  return (
+    <div
+      className={`w-64 h-32 bg-${color} rounded-lg flex flex-col gap-1 items-center justify-center border-3d border-2 border-slate-800`}
+    >
+      <h2 className="text-4xl font-bold">{count}</h2>
+      <h1 className="text-md font-bold text-gray-700">{name}</h1>
+    </div>
+  );
 };
 
 export default StatsBox;
