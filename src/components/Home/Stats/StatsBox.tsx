b8a5c5377441df3c2f93f0b503d38240d358a0cf
Lukas Haible <lukas.haible@web.de>
2025-08-20 16:28:05 +0200

    Design

--- src/components/Home/Stats/StatsBox.tsx
+++ src/components/Home/Stats/StatsBox.tsx
@@ -1,3 +1,4 @@
+import CountUp from "react-countup";
 import "../../../App.css";
 
 interface Props {
@@ -11,7 +12,9 @@ const StatsBox = ({ name, color = "white", count = 0 }: Props) => {
     <div
       className={`w-64 h-32 bg-${color} rounded-lg flex flex-col gap-1 items-center justify-center border-3d border-2 border-slate-800`}
     >
-      <h2 className="text-4xl font-bold">{count}</h2>
+      <h2 className="text-4xl font-bold">
+        <CountUp end={count} />
+      </h2>
       <h1 className="text-md font-bold text-gray-700">{name}</h1>
     </div>
   );
