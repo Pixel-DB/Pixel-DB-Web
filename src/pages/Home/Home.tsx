d427aa0799709b7144a80ea8a8da26b05898a5c1
Lukas <lukas.haible@web.de>
2025-08-21 20:22:59 +0200

    Design Feature-Box + Grid

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/components/Home/Banner/Banner";
+import FeaturesContainer from "@/components/Home/Features/FeaturesContainer";
 import StatsContainer from "@/components/Home/Stats/StatsContainer";
 
 const Home = () => {
@@ -6,6 +7,9 @@ const Home = () => {
     <div className="flex flex-col">
       <div className="flex items-center justify-center w-full">
         <Banner />
+      </div>
+      <div>
+        <FeaturesContainer />
       </div>
       <div>
         <StatsContainer />
