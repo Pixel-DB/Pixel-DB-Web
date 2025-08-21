1f1ffc6120f30e68961fb06b921902f0a4ffd55c
Lukas Haible <lukas.haible@web.de>
2025-08-21 21:25:49 +0200

    Design

--- src/components/Home/Features/FeaturesBox.tsx
+++ src/components/Home/Features/FeaturesBox.tsx
@@ -11,9 +11,14 @@ const FeaturesBox = ({ title, subtitle, color }: Props) => {
     <div
       className={`bg-${color} w-80 h-48 border-3d border-2 border-slate-800 rounded-lg`}
     >
-      <div className="flex flex-col text-center">
+      <div className="flex flex-col gap-1 text-center">
         <h1 className="text-3xl font-bold">{title}</h1>
-        <h2 className="text-md text-gray-700">{subtitle}</h2>
+        <h2
+          className="text-md text-gray-700 font-bold p-2
+        "
+        >
+          {subtitle}
+        </h2>
       </div>
     </div>
   );
