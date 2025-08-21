dc01d71cfd8fc201f2479e0099c14419df372e3a
Lukas Haible <info@brainlesslukas.xyz>
2025-08-21 11:28:02 +0200

    Profile

--- src/pages/Dashboard/DashboardProfile.tsx
+++ src/pages/Dashboard/DashboardProfile.tsx
@@ -1,9 +1,9 @@
-import useUser from "@/hooks/useUser";
 import "../../App.css";
 import Input from "@/components/ui/TextInput/Input";
 import Button from "@/components/ui/Button/Button";
 import useUpdateUser from "@/hooks/useUpdateUser";
 import { useForm, type SubmitHandler } from "react-hook-form";
+import { useUserContext } from "@/context/UserContext";
 
 interface FieldValues {
   email?: string;
@@ -14,13 +14,12 @@ interface FieldValues {
 }
 
 const DashboardProfile = () => {
-  const { userData } = useUser();
+  const { user } = useUserContext();
   const { updateUser } = useUpdateUser();
   const { register, handleSubmit } = useForm<FieldValues>();
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     updateUser(data);
-    console.log(data);
   };
 
   return (
@@ -33,38 +32,38 @@ const DashboardProfile = () => {
         <div className="flex flex-row items-center space-x-4">
           <img
             className="rounded-md"
-            src={`https://ui-avatars.com/api/?background=16a34a&name=${userData?.FirstName}+${userData?.LastName}&size=100`}
+            src={`https://ui-avatars.com/api/?background=16a34a&name=${user?.FirstName}+${user?.LastName}&size=100`}
           />
           <div className="flex flex-col">
             <h1 className="text-2xl font-bold">
-              {userData?.FirstName} {userData?.LastName}
+              {user?.FirstName} {user?.LastName}
             </h1>
-            <h1 className="text-xl">{userData?.Username}</h1>
-            <h1 className="text-sm text-gray-700">{userData?.ID}</h1>
+            <h1 className="text-xl">{user?.Username}</h1>
+            <h1 className="text-sm text-gray-700">{user?.ID}</h1>
           </div>
         </div>
         <Input
           type="email"
-          placeholder={userData?.Email}
+          placeholder={user?.Email}
           label="Edit email"
           {...register("email")}
         />
         <Input
           type="username"
-          placeholder={userData?.Username}
+          placeholder={user?.Username}
           label="Edit username"
           {...register("username")}
         />
         <div className="flex flex-row w-full space-x-4">
           <Input
             type="firstName"
-            placeholder={userData?.FirstName}
+            placeholder={user?.FirstName}
             label="Edit first name"
             {...register("firstName")}
           />
           <Input
             type="lastName"
-            placeholder={userData?.LastName}
+            placeholder={user?.LastName}
             label="Edit last name"
             {...register("lastName")}
           />
