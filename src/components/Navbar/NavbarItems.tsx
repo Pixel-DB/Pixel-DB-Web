49a203e5055de7dfd9b2c384d41f6d4345254238
Lukas Haible <lukas.haible@web.de>
2025-08-22 20:46:02 +0200

    HashLink

--- src/components/Navbar/NavbarItems.tsx
+++ src/components/Navbar/NavbarItems.tsx
@@ -1,11 +1,23 @@
 import { NavLink } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 interface Props {
   name: string;
   link: string;
+  hashlink?: boolean;
 }
 
-export const NavbarItems = ({ name, link }: Props) => {
+export const NavbarItems = ({ name, link, hashlink }: Props) => {
+  if (hashlink) {
+    return (
+      <div className="text-xl font-medium">
+        <HashLink smoth to={link}>
+          <span>{name}</span>
+        </HashLink>
+      </div>
+    );
+  }
+
   return (
     <div className="text-xl font-medium">
       <NavLink to={link}>
