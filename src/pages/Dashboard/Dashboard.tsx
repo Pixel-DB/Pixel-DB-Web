22937f6ab617a186df9e4a110247c0e3f731dd07
Lukas Haible <lukas.haible@web.de>
2025-08-22 16:27:51 +0200

    Update Colors

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
   const { user } = useUserContext();
 
   return (
-    <div className="px-7 py-4 border-3d border-2 border-gray-700 rounded-md text-2xl font-bold my-10">
+    <div className="px-7 py-4 border-3d border-2 border-gray-700 rounded-md text-2xl font-bold my-10 bg-white">
       Welcome, {user?.Username}
     </div>
   );
