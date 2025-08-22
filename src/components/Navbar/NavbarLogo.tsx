9238505ae3d0debaaf3e1df1490b99335cc66ad2
Lukas Haible <lukas.haible@web.de>
2025-08-22 19:22:59 +0200

    Update Logo

--- src/components/Navbar/NavbarLogo.tsx
+++ src/components/Navbar/NavbarLogo.tsx
@@ -3,7 +3,7 @@ import Logo from "../../assets/ColorPalatte.svg";
 
 const NavbarLogo = () => {
   return (
-    <Link className="p-2 flex items-center justify-center flex-1" to={"/"}>
+    <Link className="p-2 flex items-center justify-center" to={"/"}>
       <img src={Logo} className="w-[50px]" />
       <h1 className="pl-1 font-extrabold text-2xl">PixelDB</h1>
     </Link>
