f036f5f8c99d655c4304c5ad47d32a43f9b57cd1
Lukas Haible <info@brainlesslukas.xyz>
2025-08-23 23:46:12 +0200

    Add Close Arrow

--- src/components/Dashboard/DashboardSideBar.tsx
+++ src/components/Dashboard/DashboardSideBar.tsx
@@ -1,9 +1,13 @@
 import DashboardSideBarButtons from "./DashboardSideBarButtons";
+import { FaArrowLeft } from "react-icons/fa6";
 
 const DashboardSideBar = () => {
   return (
     <div className="flex flex-col gap-8 p-4 text-xl text-gray-800">
-      <h1 className="text-2xl font-bold py-4 text-black">Dashboard</h1>
+      <div className="flex flex-row items-center justify-between">
+        <h1 className="text-2xl font-bold py-4 text-black">Dashboard</h1>
+        <FaArrowLeft className="text-text-secondary mb-1" />
+      </div>
       <DashboardSideBarButtons to="/dashboard" end>
         Home
       </DashboardSideBarButtons>
--- src/pages/Dashboard/DashboardLayout.tsx
+++ src/pages/Dashboard/DashboardLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const DashboardLayout = () => {
   return (
     <div className="flex h-full w-full">
-      <aside className="z-30 max-w-[200px] w-full shadow-xl/30 h-screen mr-2 border-r-1 border-gray-700 bg-white">
+      <aside className="z-30 max-w-[210px] w-full shadow-xl/30 h-screen mr-2 border-r-1 border-gray-700 bg-white">
         <DashboardSideBar />
       </aside>
       <div className="absolute flex w-full justify-center">
