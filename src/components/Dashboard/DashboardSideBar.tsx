3f5b5b076576ef1bc6387fcd9157eb85ec582e5d
Lukas <info@brainlesslukas.xyz>
2025-08-23 23:53:39 +0200

    Close Logic

--- src/components/Dashboard/DashboardSideBar.tsx
+++ src/components/Dashboard/DashboardSideBar.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import DashboardSideBarButtons from "./DashboardSideBarButtons";
 import { FaArrowLeft } from "react-icons/fa6";
 
 const DashboardSideBar = () => {
+  const [isClosed, setIsClosed] = useState(true);
+
+  const handleClose = () => {
+    setIsClosed(!isClosed);
+    console.log(isClosed);
+  };
+
   return (
     <div className="flex flex-col gap-8 p-4 text-xl text-gray-800">
       <div className="flex flex-row items-center justify-between">
         <h1 className="text-2xl font-bold py-4 text-black">Dashboard</h1>
-        <FaArrowLeft className="text-text-secondary mb-1" />
+        <FaArrowLeft
+          className="text-text-secondary mb-1 cursor-pointer"
+          onClick={handleClose}
+        />
       </div>
       <DashboardSideBarButtons to="/dashboard" end>
         Home
