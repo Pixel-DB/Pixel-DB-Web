58f225ba8ce3231e4fc28ec45dbadb6c0d5d7c48
Lukas Haible <lukas.haible@web.de>
2025-08-24 00:29:08 +0200

    Closed Design

--- src/components/Dashboard/DashboardSideBar.tsx
+++ src/components/Dashboard/DashboardSideBar.tsx
@@ -12,37 +12,57 @@ const DashboardSideBar = () => {
 
   const handleClose = () => {
     setIsClosed(!isClosed);
-    console.log(isClosed);
   };
 
   return (
     <div className="flex flex-col gap-8 p-4 text-xl text-gray-800">
-      <div className="flex flex-row items-center justify-between gap-4">
-        <h1 className="text-2xl font-bold py-4 text-black">Dashboard</h1>
+      <div className="flex flex-row items-center justify-between gap-4 py-4">
+        <h1
+          className={`text-2xl font-bold text-black ${
+            isClosed ? "hidden" : ""
+          }`}
+        >
+          Dashboard
+        </h1>
         <FaArrowLeft
-          className="text-text-secondary mb-1 cursor-pointer"
+          className="text-text-secondary mb-1 cursor-pointer justify-center w-full"
           onClick={handleClose}
         />
       </div>
-      <DashboardSideBarButtons to="/dashboard" icon={<FaHome />} end>
+      <DashboardSideBarButtons
+        to="/dashboard"
+        icon={<FaHome />}
+        end
+        isClosed={isClosed}
+      >
         Home
       </DashboardSideBarButtons>
-      <DashboardSideBarButtons to="/dashboard/profile" icon={<IoPerson />}>
+      <DashboardSideBarButtons
+        to="/dashboard/profile"
+        icon={<IoPerson />}
+        isClosed={isClosed}
+      >
         Profile
       </DashboardSideBarButtons>
       <DashboardSideBarButtons
         to="/dashboard/upload"
         icon={<FaCloudUploadAlt />}
+        isClosed={isClosed}
       >
         Upload
       </DashboardSideBarButtons>
       <DashboardSideBarButtons
         to="/dashboard/settings"
         icon={<IoSettingsSharp />}
+        isClosed={isClosed}
       >
         Settings
       </DashboardSideBarButtons>
-      <DashboardSideBarButtons to="/dashboard/license" icon={<FaBook />}>
+      <DashboardSideBarButtons
+        to="/dashboard/license"
+        icon={<FaBook />}
+        isClosed={isClosed}
+      >
         License
       </DashboardSideBarButtons>
     </div>
