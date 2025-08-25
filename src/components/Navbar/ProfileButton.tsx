e19526f3a5cae1f18c0979fd4314e3f438abf83c
Lukas Haible <lukas.haible@web.de>
2025-08-26 00:20:05 +0200

    Link + Username + Email

--- src/components/Navbar/ProfileButton.tsx
+++ src/components/Navbar/ProfileButton.tsx
@@ -4,14 +4,9 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 const LoginButton = () => {
   const { user } = useUserContext();
 
-  const handleClick = () => {};
-
   return (
     <div className="flex w-full justify-center">
-      <div
-        className="flex flex-row rounded-md px-2 py-1 hover:bg-amber-100 items-center"
-        onClick={handleClick}
-      >
+      <div className="flex flex-row rounded-md px-2 py-1 hover:bg-amber-100 items-center">
         <img
           className="rounded-md"
           src={`https://ui-avatars.com/api/?background=16a34a&name=${user?.Username}&size=35&length=1&font-size=0.6`}
