e7b5420fa92a8e64a78374ae5fcb3d42d94ab53c
Lukas <info@brainlesslukas.xyz>
2025-08-25 22:07:06 +0200

    Dropdown Menu Button

--- src/components/Navbar/ProfileButton.tsx
+++ src/components/Navbar/ProfileButton.tsx
@@ -1,14 +1,18 @@
 import { useUserContext } from "@/context/UserContext";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 const LoginButton = () => {
   const { user } = useUserContext();
 
   return (
-    <div className="flex flex-row items-center w-full justify-center">
-      <img
-        className="rounded-md"
-        src={`https://ui-avatars.com/api/?background=16a34a&name=${user?.Username}&size=35&length=1&font-size=0.6`}
-      />
+    <div className="flex w-full justify-center">
+      <div className="flex flex-row rounded-md px-2 py-1 hover:bg-amber-100 items-center">
+        <img
+          className="rounded-md"
+          src={`https://ui-avatars.com/api/?background=16a34a&name=${user?.Username}&size=35&length=1&font-size=0.6`}
+        />
+        <RiArrowDropDownLine className="text-3xl text-black" />
+      </div>
     </div>
   );
 };
