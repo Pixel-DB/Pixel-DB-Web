37eb7eba0e45d15e6503668a25114273e50dd3a0
Lukas Haible <lukas.haible@web.de>
2025-08-25 23:05:22 +0200

    Create Dropdown Menu File

--- src/components/Navbar/ProfileButton.tsx
+++ src/components/Navbar/ProfileButton.tsx
@@ -4,9 +4,14 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 const LoginButton = () => {
   const { user } = useUserContext();
 
+  const handleClick = () => {};
+
   return (
     <div className="flex w-full justify-center">
-      <div className="flex flex-row rounded-md px-2 py-1 hover:bg-amber-100 items-center">
+      <div
+        className="flex flex-row rounded-md px-2 py-1 hover:bg-amber-100 items-center"
+        onClick={handleClick}
+      >
         <img
           className="rounded-md"
           src={`https://ui-avatars.com/api/?background=16a34a&name=${user?.Username}&size=35&length=1&font-size=0.6`}
