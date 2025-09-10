5de8912627ce18f52f078d418c312a0dce52270b
Lukas Haible <lukas.haible@web.de>
2025-09-10 17:17:10 +0200

    useUser Hook

--- src/pages/Dashboard/Admin/DashboardAdminUsers.tsx
+++ src/pages/Dashboard/Admin/DashboardAdminUsers.tsx
@@ -1,5 +1,9 @@
 const DashboardAdminUsers = () => {
-  return <div>User Moderation</div>;
+  return (
+    <div className="flex-col gap-4 w-full px-4 py-4 border-3d border-2 border-gray-700 rounded-md my-10 bg-white h-full">
+      <h1 className="w-full text-left text-2xl font-bold">User Moderation</h1>
+    </div>
+  );
 };
 
 export default DashboardAdminUsers;
