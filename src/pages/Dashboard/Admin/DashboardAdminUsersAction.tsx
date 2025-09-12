66b97ef787e877d8f5d1a56b58b43c131b49a834
“Lukas” <info@brainlesslukas.xyz>
2025-09-12 22:55:49 +0200

    Ban/Delete User Button

--- src/pages/Dashboard/Admin/DashboardAdminUsersAction.tsx
+++ src/pages/Dashboard/Admin/DashboardAdminUsersAction.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/ui/Button/Button";
 import SearchInput from "@/components/ui/SearchInput/SearchInput";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
@@ -8,9 +9,15 @@ interface FieldValues {
 
 interface Props {
   onSearchChange: (value: string) => void;
+  onBanClick?: () => void;
+  onDeleteClick?: () => void;
 }
 
-const DashboardAdminUsersAction = ({ onSearchChange }: Props) => {
+const DashboardAdminUsersAction = ({
+  onSearchChange,
+  onBanClick,
+  onDeleteClick,
+}: Props) => {
   const { register, watch } = useForm<FieldValues>();
   const search = watch("search", "");
 
@@ -19,9 +26,17 @@ const DashboardAdminUsersAction = ({ onSearchChange }: Props) => {
   }, [search, onSearchChange]);
 
   return (
-    <div className="w-full flex flex-row">
+    <div className="w-full flex flex-row justify-between items-center">
       <div className="w-1/3">
         <SearchInput placeholder="Search for User" {...register("search")} />
+      </div>
+      <div className="flex flex-row h-9 space-x-4">
+        <Button color="amber" onClick={onBanClick}>
+          Ban
+        </Button>
+        <Button color="red" onClick={onDeleteClick}>
+          Delete
+        </Button>
       </div>
     </div>
   );
