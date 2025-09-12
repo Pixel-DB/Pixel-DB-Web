8a26e839c68533e648ea9a245f71d3c222d4ca58
“Lukas” <info@brainlesslukas.xyz>
2025-09-12 15:59:52 +0200

    Layout

--- src/pages/Dashboard/Admin/DashboardAdminUsersAction.tsx
+++ src/pages/Dashboard/Admin/DashboardAdminUsersAction.tsx
@@ -3,7 +3,9 @@ import SearchInput from "@/components/ui/SearchInput/SearchInput";
 const DashboardAdminUsersAction = () => {
   return (
     <div className="w-full flex flex-row">
-      <SearchInput placeholder="Search for User" />
+      <div className="w-1/3">
+        <SearchInput placeholder="Search for User" />
+      </div>
     </div>
   );
 };
